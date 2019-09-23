6d9d5a3c042a77da9082ff1dd8ce0e85af26384a
nickroh <48642955+nickroh@users.noreply.github.com>
2019-09-23 23:32:00 +0900

    Create hw.java

---
+++ hw.java
@@ -0,0 +1,111 @@
+import chn.util.*;
+
+
+public class chn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		ConsoleIO a = new ConsoleIO();
+//		System.out.println("Number..");
+//		int num = a.readInt();
+//		System.out.println(num);
+		
+		double sum=0;
+		double avg;
+		
+		Data[] data = new Data[1000];
+		double[] stat = new double[200];
+		double[] getans;
+		
+		FileInput inFile = new FileInput("C:\\Users\\roh51\\Desktop\\STUDY\\AP Computer Science\\apcs.txt"); // 저장해논 파일 경로
+		int n;
+		n = inFile.readInt();
+		
+		
+		
+//		System.out.println(n);
+//		for(int i=0; i<n ;i++)
+//		{
+//			tmp[i] = inFile.readToken();
+//			tmp2[i]= inFile.readToken();
+//		}
+//		System.out.println(tmp[1]);
+		
+		for(int i=0; i<n ;i++)
+		{
+			data[i]=new Data();
+			
+			data[i].name = inFile.readToken();
+			data[i].a = inFile.readDouble();
+			data[i].b = inFile.readDouble();
+			data[i].c = inFile.readDouble();
+		}
+//		for(int i=0; i<n ;i++)
+//		{
+//			System.out.println(tmp[i]);
+//			System.out.println(tmp2[i]);
+//		}
+//		
+		for(int i=0;i<n;i++)
+		{
+			data[i].score = score(data[i].a,data[i].b,data[i].c);
+		}
+		
+		
+		
+		for(int i=0; i<n;i++)
+		{
+			stat[i]=data[i].score;
+			sum += stat[i];
+		}
+		avg = sum/n;
+		getans = statistic(stat,avg,n);
+		
+		for(int i=0; i < n;i++)
+		{
+			System.out.printf("%s 의 성적은  %.1f\n",data[i].name,data[i].score);
+		}
+		System.out.println("<----------------------------------->");
+		System.out.printf("분산과 표준편차는 %.1f 와 %.1f 입니다",getans[0],getans[1]);
+		 
+	}
+
+		
+	public static double score( double a,  double b, double c)
+	{
+		double score=0;
+		
+		score = a*0.35f + b*0.35f + c*0.3f;
+		
+		return score;
+	}
+	
+	public static double[] statistic(double[] arr, double a, int n)
+	{
+		double[] ans = new double[2];
+		double total=0;
+		
+		for(int i=0;i<n;i++)
+		{
+			double tmp = arr[i] - a;
+			total += tmp * tmp;
+		}
+		ans[0]= total / n;
+		ans[1] = Math.sqrt(ans[0]);
+		return ans;
+	}
+	
+}
+
+class Data{
+	String name;
+	double a;
+	double b;
+	double c;
+	double score;
+	
+	public Data()
+	{
+	
+	}
+}
