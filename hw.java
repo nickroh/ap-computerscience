7add763ea56d86be8c7bfccaf8b4af66439a2149
nickroh <48642955+nickroh@users.noreply.github.com>
2019-09-24 09:03:32 +0900

    Update hw.java

--- hw.java
+++ hw.java
@@ -1,3 +1,12 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@FreeBSD.ORG> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+ */
+
 //input file 예시와 output file 에시는 sample 폴더 안에있음 https://github.com/nickroh/ap-computerscience/tree/master/sample
 // 중간 기말 수행 35%/35%/30% 라고 가정
 
