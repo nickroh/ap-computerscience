6f36ff9d43d2e94153fd255eabe756ee46968a1a
nickroh <48642955+nickroh@users.noreply.github.com>
2019-11-23 20:19:21 +0900

    Update Flood.java

--- recursion/Flood.java
+++ recursion/Flood.java
@@ -1,9 +1,10 @@
+
 public class Flood{
     public static void main(String args[]){
         map a = new map();
         a.printmap();
-        System.out.println("> file.encoding = " + System.getProperty("file.encoding"));
-
+        a.traverse(2,1);
+        a.order();
 
     }
 }
