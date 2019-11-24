68592f9adaa8ae142fc4f4ab6f423b5bde7c3d77
nickroh <48642955+nickroh@users.noreply.github.com>
2019-11-25 00:12:35 +0900

    Update Flood.java

--- recursion/Flood.java
+++ recursion/Flood.java
@@ -1,10 +1,43 @@
+import java.util.*;
 
 public class Flood{
     public static void main(String args[]){
-        map a = new map();
-        a.printmap();
-        a.traverse(2,1);
-        a.order();
 
+        Scanner sc = new Scanner(System.in);
+        map a = new map();
+        int x;
+        int y;
+
+        System.out.println("Start From Random Point? Y/N");
+        String input = sc.next();
+        char input_1 = input.charAt(0);
+        if(input_1 == 'y' || input_1 == 'Y'){
+            x = start_point();
+            y = start_point();
+            
+            a.printmap();
+            a.traverse(x, y);
+            a.order();
+
+        }
+        else if(input_1 == 'n' || input_1 == 'N'){
+            a.printmap();
+            a.traverse(2,1);
+            a.order();
+        }
+        else{
+            System.out.println("Invalid Input");
+        }
+        
+
+    }
+
+    private static int start_point(){
+        
+        int a;
+
+        a = (int)(Math.random()*9.0);
+        a+=1;
+        return a;
     }
 }
