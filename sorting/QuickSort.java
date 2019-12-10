6724af3e4cc6311b0a0b03616b57bdad436102eb
nickroh <48642955+nickroh@users.noreply.github.com>
2019-12-10 09:56:45 +0900

    Update QuickSort.java

--- sorting/QuickSort.java
+++ sorting/QuickSort.java
@@ -1,19 +1,29 @@
-import java.util.Collections;
 import java.util.Arrays;
 
 public class QuickSort{
     public static void sort(int []arr){
-        Collections.shuffle(Arrays.asList(arr));
-        
+        arr = shuffle(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
         quick_sort(arr, 0, arr.length - 1);
     }
-
+    public static int[] shuffle(int[] arr){
+        for(int x=0;x<arr.length;x++){
+          int i = (int)(Math.random()*arr.length);
+          int j = (int)(Math.random()*arr.length);
+                
+          int tmp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = tmp;
+        }     
+        return arr;
+    }
     private static void swap(int[] arr, int a, int b) {
         int tmp = arr[a];
         arr[a] = arr[b];
         arr[b] = tmp;
     }
-
     private static void quick_sort(int[] arr, int f, int l) {
         int cnt = l - f;
         if (cnt <= 0) {
@@ -22,11 +32,11 @@ public class QuickSort{
         int left = f;
         int right = l-1;
         int point = arr[l];
-        
+        // System.out.println("point="+ point);
         point = arr[l];
         
         while (true) {
-            while (arr[left] < point && left<=right) {
+            while (arr[left] <= point && left<=right) {
                 left++;
             }
             while (arr[right] > point && left<right ) {
@@ -38,16 +48,17 @@ public class QuickSort{
             swap(arr, left, right);
         }
         swap(arr, left, l);
-        // for(int i=0;i<15;i++){
+        // for(int i=0;i<arr.length;i++){
         //     System.out.printf("%d ",arr[i]);
         // }
         // System.out.println();
+        // System.out.println();
 
-        // System.out.println(l);
-        // System.out.println(f);
-        // System.out.println(left);
-        quick_sort(arr, f, left -1);
-        
+        // System.out.println("l="+l);
+        // System.out.println("f="+f);
+        // System.out.println("left="+left);
+        // System.out.println("point="+point);
+        quick_sort(arr, f, left -1);       
         quick_sort(arr ,left + 1, l );
     }
     
