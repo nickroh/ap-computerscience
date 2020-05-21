8e1dac44ab20babdb087a030b1f5346bf3566ad1
nickroh <48642955+nickroh@users.noreply.github.com>
2020-05-22 01:27:33 +0900

    Update InsertionSort.java

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -1,37 +1,64 @@
-public class InsertionSort{
+public class SelectionSort{
 	
 	public static void sort(int[] arr){
 		
 		int cnt = arr.length;
 
-		for (int i = 1; i < cnt; i++){
-        
-            int key = arr[i];
-		    for (int j = 1; j <= i; j++){
-			    if (arr[i - j] > key){
-				    arr[i - j + 1] = arr[i - j];
-			    	arr[i - j] = key;
-			    }else{
-			    	break;
-			    }
-		    }
-	    }
+		for (int i = 0; i < cnt; i++){
+		
+		int min = arr[i];
+		
+			for (int j = i+1; j < cnt; j++){
+				if (min > arr[j]){
+				
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+					min = arr[i];
+			
+				}
+			}
+		}
+	}
+	public static void sort2(int[] arr){
+		int cnt = arr.length;
+
+		for(int i=0;i<cnt;i++){
+			int tmp = arr[0];
+			int checkpoint = 0;
+			for(int j=0;j<cnt-i;j++){
+				if(tmp<arr[j]){
+					tmp = arr[j];
+					checkpoint = j;
+				}
+			}
+			
+			for(int k=checkpoint+1;k<cnt-i;k++){
+				arr[k-1]=arr[k];
+			}
+			arr[cnt - i-1] = tmp;
+		}
+
+
 	}
 	public static void reverse_sort(int[] arr){
 		
 		int cnt = arr.length;
 
-		for (int i = cnt-2; i >= 0; i--){
-        
-            int key = arr[i];
-		    for (int j = 1; j <= cnt-1-i ; j++){
-			    if (arr[i + j] > key){
-				    arr[i + j -1 ] = arr[i + j];
-				    arr[i + j] = key;
-			    }else{
-			    	break;
-			    }
-		    }
-	    }
+		for (int i = cnt-1; i >= 0; i--){
+		
+		int min = arr[i];
+		
+			for (int j = i-1; j >= 0; j--){
+				if (min > arr[j]){
+				
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+					min = arr[i];
+			
+				}
+			}
+		}
 	}
 }
